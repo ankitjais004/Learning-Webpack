575d252055c59b88295691b363cc23f067467f3b
Ankit Jaiswal <ajaiswal@kreeti.com>
2015-10-30 13:47:56 +0530

    Note building  App

---
+++ app/components/App.jsx
@@ -0,0 +1,78 @@
+import uuid from 'node-uuid';
+import React from 'react';
+import Notes from './Notes.jsx';
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Bring Vegetables'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Pay electricity bill'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Car washing'
+        }
+      ]
+    };
+    this.addNote = this.addNote.bind(this);
+    this.findNote = this.findNote.bind(this);
+    this.editNote = this.editNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+  }
+
+  render() {
+    const notes = this.state.notes;
+
+    return (
+      <div>
+      <h1> Add Your Important Notes Here </h1>
+      <button className="add-note" onClick={this.addNote}>+</button>
+      <Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+      </div>
+    );
+  }
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+  editNote(id, task) {
+    let notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+    if(noteIndex < 0) {
+      return;
+      }
+      notes[noteIndex].task = task;
+      this.setState({notes});
+    }
+  findNote(id) {
+    const notes = this.state.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
+    if(noteIndex < 0) {
+      console.warn('Failed to find note', notes, id);
+    }
+    return noteIndex;
+  }
+  deleteNote(id) {
+    const notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+
+    if(noteIndex <0){
+      return;
+    }
+    this.setState({
+      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
+    });
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
-  entry: APP_PATH,
+var common = {
+  entry: './app/index.jsx',
+  resolve: {
+    extension: ['', '.js','.jsx']
+    },
   output: {
     path: BUILD_PATH,
     filename: 'bundle.js'
@@ -17,12 +22,32 @@ module.exports = {
     test: /\.css$/,
     loaders: ['style', 'css'],
     include: APP_PATH
-        }
+        },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
       ]
   },
-  plugin:[
-   new HtmlwebpackPlugin({
-     title: 'Kanban app'
-   })
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
   ]
-};
+  };
+
+ if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+   }
